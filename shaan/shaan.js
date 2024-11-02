57c274ac2a7f66b126d82422f2b84a5676e9eebd
Olivier Bado-Faustin <12731381+Badatos@users.noreply.github.com>
2024-11-02 18:44:09 +0100

    Add a necrosis test

--- shaan/shaan.js
+++ shaan/shaan.js
@@ -1,8 +1,16 @@
+/**
+    SHAAN RPG Dice Roller scripts
+**/
 
-
+// Nombre d'itérations avant de stopper les dés
 var roll_count = 10;
+
+// Dossier des images
 const img_url = "img/";
+
+// Valeurs de thrines obtenues
 var esprit, ame, corps, necrose;
+
 // Blocs d'alertes
 const symbiose = document.getElementById("symbiose");
 const symbiose_necro = document.getElementById("symbiose-necro");
@@ -12,18 +20,55 @@ const bonus = document.getElementById("bonus");
 const exp = document.getElementById("exp");
 const limbes = document.getElementById("limbes");
 const perte_bloc = document.getElementById("perte");
+const critical_necrosis = document.getElementById("critical-necrosis");
+const necrosis_help = document.getElementById("necrosis-help");
+
 // Dés
-var de_esprit = document.querySelector("#de_esprit>img");
-var de_ame = document.querySelector("#de_ame>img");
-var de_corps = document.querySelector("#de_corps>img");
-var de_necrose = document.querySelector("#de_necrose>img");
+const de_esprit = document.getElementById("de_esprit");
+const img_esprit = de_esprit.querySelector("img");
+
+const de_ame = document.getElementById("de_ame");
+const img_ame = de_ame.querySelector("img");
+const txt_ame = document.getElementById("de_trihn_2");
+
+const de_corps = document.getElementById("de_corps");
+const img_corps = de_corps.querySelector("img");
+
+const de_necrose = document.getElementById("de_necrose");
+const img_necrose = de_necrose.querySelector("img");
+const txt_necrose = document.getElementById("de_trihn_4");
+
 // Perte
 const num_perte = document.getElementById("num_perte");
 
 /**
- * Réinitialise les blocs d'infos, et lance les dés.
+ * Lance les dés pour un test de domaine.
  */
-function rollDices() {
+function domain_test() {
+    reset_blocs();
+    de_esprit.classList.remove("d-none");
+    de_corps.classList.remove("d-none");
+    txt_necrose.textContent = "Perte";
+    txt_ame.textContent = "Âme";
+    random_domain();
+}
+
+/**
+ * Prépare les dés et lance un test de nécrose.
+ */
+function necrosis_test() {
+    reset_blocs();
+    de_esprit.classList.add("d-none");
+    de_corps.classList.add("d-none");
+    txt_necrose.textContent = "Nécrose";
+    txt_ame.textContent = "Perte";
+    random_necrosis();
+}
+
+/**
+ * Réinitialise les blocs d'infos, et lance les dés pour un test de domaine.
+ */
+function reset_blocs() {
     symbiose.classList.add("d-none");
     symbiose_necro.classList.add("d-none");
     symbiose_necro_trans.classList.add("d-none");
@@ -31,8 +76,9 @@ function rollDices() {
     bonus.classList.add("d-none");
     exp.classList.add("d-none");
     limbes.classList.add("d-none");
+    critical_necrosis.classList.add("d-none");
+    necrosis_help.classList.add("d-none");
     // perte_bloc.classList.add("d-none");
-    RandomDices();
 }
 
 /**
@@ -40,13 +86,13 @@ function rollDices() {
  * (pour donner un effet visuel aux lancement des dés)
  * Puis affiche les alertes éventuelles.
  */
-function RandomDices() {
+function random_domain() {
 
-    RollDice();
+    roll_domain();
 
     roll_count--;
 
-    if (roll_count > 0) window.setTimeout(RandomDices, 100);
+    if (roll_count > 0) window.setTimeout(random_domain, 100);
     else {
         roll_count = 10;
         if (esprit == 0 && ame == 0 && corps == 0 && necrose == 0) {
@@ -77,7 +123,6 @@ function RandomDices() {
                     }
                 }
             }
-
         }
         // Calcul des pertes
         if (necrose == 0) {
@@ -90,30 +135,81 @@ function RandomDices() {
         num_perte.textContent = perte;
         perte_bloc.classList.remove("d-none");
     }
+}
 
+/**
+ * Effectue un test de nécrose
+ */
+function random_necrosis() {
+
+    roll_necrosis();
+
+    roll_count--;
+
+    if (roll_count > 0) window.setTimeout(random_necrosis, 100);
+    else {
+        roll_count = 10;
+        if (necrose == 0) {
+            critical_necrosis.classList.remove("d-none");
+        } else {
+            necrosis_help.classList.remove("d-none");
+        }
+        // Calcul des pertes
+        if (ame == 0) {
+            perte = 3;
+        } else if (ame > 5) {
+            perte = 2;
+        } else {
+            perte = 1;
+        }
+        num_perte.textContent = perte;
+        perte_bloc.classList.remove("d-none");
+    }
 }
 
 /**
  * Génére une combinaison aléatoire des 4 dés de Shaan.
  */
-function RollDice() {
-    esprit = Math.floor(Math.random() * 10);
-    de_esprit.src = img_url + "d10/d10_jaune_" + esprit + ".png";
-    de_esprit.className = "score_" + esprit;
-    de_esprit.alt = esprit;
-
-    ame = Math.floor(Math.random() * 10);
-    de_ame.src = img_url + "d10/d10_bleu_" + ame + ".png";
-    de_ame.className = "score_" + ame;
-    de_ame.alt = ame;
-
-    corps = Math.floor(Math.random() * 10);
-    de_corps.src = img_url + "d10/d10_rouge_" + corps + ".png";
-    de_corps.className = "score_" + corps;
-    de_corps.alt = corps;
-
-    necrose = Math.floor(Math.random() * 10);
-    de_necrose.src = img_url + "d10/d10_noir_" + necrose + ".png";
-    de_necrose.className = "score_" + necrose;
-    de_necrose.alt = necrose;
+function roll_domain() {
+    esprit = roll_dice(img_esprit, "d10/d10_jaune_");
+    ame = roll_dice(img_ame, "d10/d10_bleu_");
+    corps = roll_dice(img_corps, "d10/d10_rouge_");
+    necrose = roll_dice(img_necrose, "d10/d10_noir_");
 }
+
+/**
+ * Génére une combinaison aléatoire des dés bleu et noir.
+ */
+function roll_necrosis() {
+    ame = roll_dice(img_ame, "d10/d10_bleu_");
+    necrose = roll_dice(img_necrose, "d10/d10_noir_");
+}
+
+/**
+ * Relance le dé noir
+ */
+function reroll_black() {
+    necrosis_help.classList.add("d-none");
+    necrose = roll_dice(img_necrose, "d10/d10_noir_");
+    roll_count--;
+
+    if (roll_count > 0) window.setTimeout(reroll_black, 100);
+    else {
+        roll_count = 10;
+        if (necrose == 0) {
+            critical_necrosis.classList.remove("d-none");
+        }
+    }
+}
+
+/**
+ * Lance un dé.
+ */
+function roll_dice(img, color) {
+    value = Math.floor(Math.random() * 10);
+    img.src = img_url + color + value + ".png";
+    img.className = "score_" + value;
+    img.alt = necrose;
+    return value;
+}
+
