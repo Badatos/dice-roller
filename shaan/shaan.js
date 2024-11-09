bf6d911f03e907fb006df3509bd572628c7d9888
Olivier Bado-Faustin <12731381+Badatos@users.noreply.github.com>
2024-11-09 17:03:47 +0100

    Possibility to disable one dice

--- shaan/shaan.js
+++ shaan/shaan.js
@@ -9,7 +9,10 @@ var roll_count = 10;
 const img_url = "img/";
 
 // Valeurs de thrines obtenues
-var esprit, ame, corps, necrose;
+var thrin_values = { 'jaune': 0, 'bleu': 0, 'rouge': 0, 'noir': 0 }
+
+// Boutons d'action
+const domain_btn = document.getElementById("domain-btn");
 
 // Blocs d’alertes
 const symbiose = document.getElementById("symbiose");
@@ -29,19 +32,13 @@ const generic_title = document.getElementById("generic-title");
 const generic_desc = document.getElementById("generic-desc");
 
 // Dés
-const de_esprit = document.getElementById("de_esprit");
-const img_esprit = de_esprit.querySelector("img");
-
-const de_ame = document.getElementById("de_ame");
-const img_ame = de_ame.querySelector("img");
-const txt_ame = document.getElementById("de_trihn_2");
-
-const de_corps = document.getElementById("de_corps");
-const img_corps = de_corps.querySelector("img");
+const de_esprit = document.getElementById("de-esprit");
+const de_ame = document.getElementById("de-ame");
+const de_corps = document.getElementById("de-corps");
 const txt_corps = document.getElementById("de_trihn_3");
-
-const de_necrose = document.getElementById("de_necrose");
-const img_necrose = de_necrose.querySelector("img");
+const de_perte = document.getElementById("de-perte-bleu");
+const de_necrose = document.getElementById("de-necrose");
+const de_failure = document.getElementById("de-failure");
 const txt_necrose = document.getElementById("de_trihn_4");
 
 // Perte
@@ -54,8 +51,8 @@ function domain_test() {
     reset_blocs();
     de_esprit.classList.remove("d-none");
     de_corps.classList.remove("d-none");
+    de_ame.classList.remove("d-none");
     txt_necrose.textContent = "Perte";
-    txt_ame.textContent = "Âme";
     random_domain();
 }
 
@@ -66,8 +63,9 @@ function necrosis_test() {
     reset_blocs();
     de_esprit.classList.add("d-none");
     de_corps.classList.add("d-none");
+    de_ame.classList.add("d-none");
+    de_perte.classList.remove("d-none");
     txt_necrose.textContent = "Nécrose";
-    txt_ame.textContent = "Perte";
     random_necrosis();
 }
 
@@ -78,10 +76,10 @@ function random_failure() {
     reset_blocs();
     de_esprit.classList.add("d-none");
     de_ame.classList.add("d-none");
+    de_corps.classList.add("d-none");
     de_necrose.classList.add("d-none");
-    de_corps.classList.remove("d-none");
     perte_bloc.classList.add("d-none");
-    txt_corps.textContent = "Dé d’échec";
+    de_failure.classList.remove("d-none");
     roll_failure();
 }
 
@@ -103,8 +101,8 @@ function reset_blocs() {
     generic_help.classList.add("d-none");
     de_ame.classList.remove("d-none");
     de_necrose.classList.remove("d-none");
-    txt_corps.textContent = "Corps";
-    // perte_bloc.classList.add("d-none");
+    de_perte.classList.add("d-none");
+    de_failure.classList.add("d-none");
 }
 
 /**
@@ -120,22 +118,26 @@ function random_domain() {
 
     if (roll_count > 0) window.setTimeout(random_domain, 100);
     else {
+        let esprit = thrin_values["jaune"];
+        let ame = thrin_values["bleu"];
+        let corps = thrin_values["rouge"];
+        let necrose = thrin_values["noir"];
         roll_count = 10;
         // Symbiose nécrotique transcendantale
-        if (esprit == 0 && ame == 0 && corps == 0 && necrose == 0) {
+        if (esprit === 0 && ame === 0 && corps === 0 && necrose === 0) {
             symbiose_necro_trans.classList.remove("d-none");
             exp.classList.remove("d-none");
         } else {
             // Symbiose transcendantale
-            if (esprit == ame && ame == corps && ame == necrose) {
+            if (esprit === ame && ame === corps && ame === necrose) {
                 symbiose_trans.classList.remove("d-none");
                 exp.classList.remove("d-none");
             } else {
                 // Symbiose nécrotique
-                if ((necrose == ame && necrose == corps) ||
-                    (necrose == ame && necrose == esprit) ||
-                    (necrose == esprit && necrose == corps) ||
-                    (esprit == 0 && ame == 0 && corps == 0)) {
+                if ((necrose === ame && necrose === corps) ||
+                    (necrose === ame && necrose === esprit) ||
+                    (necrose === esprit && necrose === corps) ||
+                    (esprit === 0 && ame === 0 && corps === 0)) {
                     symbiose_necro.classList.remove("d-none");
                     exp.classList.remove("d-none");
                 } else {
@@ -177,7 +179,8 @@ function random_domain() {
  */
 function random_necrosis() {
 
-    roll_necrosis();
+    let bleu = roll_dice(de_perte, "bleu");
+    let necrose = roll_dice(de_necrose,"noir");
 
     roll_count--;
 
@@ -188,9 +191,11 @@ function random_necrosis() {
             critical_necrosis.classList.remove("d-none");
         } else {
             necrosis_help.classList.remove("d-none");
+            failure_help.classList.remove("d-none");
         }
+
         // Calcul des pertes
-        if (ame == 0) {
+        if (bleu == 0) {
             perte = 3;
         } else if (ame > 5) {
             perte = 2;
@@ -206,18 +211,15 @@ function random_necrosis() {
  * Génére une combinaison aléatoire des 4 dés de Shaan.
  */
 function roll_domain() {
-    esprit = roll_dice(img_esprit, "jaune");
-    ame = roll_dice(img_ame, "bleu");
-    corps = roll_dice(img_corps, "rouge");
-    necrose = roll_dice(img_necrose, "noir");
-}
-
-/**
- * Génére une combinaison aléatoire des dés bleu et noir.
- */
-function roll_necrosis() {
-    ame = roll_dice(img_ame, "bleu");
-    necrose = roll_dice(img_necrose, "noir");
+    if (thrin_values["jaune"] >= 0) {
+        roll_dice(de_esprit, "jaune");
+    }
+    if (thrin_values["bleu"] >= 0) {
+        roll_dice(de_ame, "bleu");
+    }
+    if (thrin_values["rouge"] >= 0) {
+        roll_dice(de_corps, "rouge");
+    }
 }
 
 /**
@@ -225,7 +227,7 @@ function roll_necrosis() {
  */
 function reroll_black() {
     necrosis_help.classList.add("d-none");
-    necrose = roll_dice(img_necrose, "noir");
+    necrose = roll_dice(de_necrose, "noir");
     roll_count--;
 
     if (roll_count > 0) window.setTimeout(reroll_black, 100);
@@ -233,6 +235,7 @@ function reroll_black() {
         roll_count = 10;
         if (necrose == 0) {
             critical_necrosis.classList.remove("d-none");
+            failure_help.classList.add("d-none");
         }
     }
 }
@@ -241,7 +244,7 @@ function reroll_black() {
  * Lance le dé rouge pour déterminer un échec personnel
  */
 function roll_failure() {
-    failure = roll_dice(img_corps, "rouge");
+    failure = roll_dice(de_failure, "rouge");
     roll_count--;
 
     if (roll_count > 0) window.setTimeout(roll_failure, 100);
@@ -303,18 +306,48 @@ function roll_failure() {
         }
         generic_help.classList.remove("d-none");
         generic_title.textContent = `(${failure}) ${title}`;
-        generic_desc.textContent = msg;
+        msg += "<hr><p>Tout échec personnel vous fait gagner 1px</p>";
+        generic_desc.innerHTML = msg;
     }
 }
 
 /**
  * Lance un dé.
  */
-function roll_dice(img, color) {
+function roll_dice(container, color) {
+    let img = container.querySelector("img");
     value = Math.floor(Math.random() * 10);
     img.src = img_url + "d10/d10_" + color + "_" + value + ".png";
     img.className = "score_" + value;
     img.alt = "" + value + " au dé " + color;
+    thrin_values[color] = value;
+    img.title = "" + thrin_values[color] + " au dé " + color;
     return value;
 }
 
+/** Listen for toggle event on each colored dice */
+document.querySelectorAll(".des>input").forEach((input) => {
+    input.addEventListener('change', (event) => {
+        let parent = event.target.parentNode;
+        let color = parent.dataset.color;
+        let checked = event.target.checked;
+        if (!checked) {
+            thrin_values[color] = -1;
+        } else if (thrin_values[color] === -1) {
+            thrin_values[color] = 0;
+        }
+        parent.classList.toggle("disabled");
+        parent.querySelector(".disabled_txt").innerHTML = checked ? "&nbsp;" : "(désactivé)";
+        // TODO: si aucun dé actif, désactiver le test de domaine.
+        if (thrin_values["jaune"] + thrin_values["bleu"] + thrin_values["rouge"] < -2) {
+            domain_btn.classList.add("disabled");
+            domain_btn.title = "Il faut au moins une énergie pour faire un test de domaine.";
+        } else {
+            domain_btn.classList.remove("disabled");
+            domain_btn.title = "Faire un test de domaine en lancant tous les dés.";
+        }
+    });
+});
+
+// TODO : retirer un dé. ==> seule la symbiose nécro reste possible
+// retirer 2 dés : plus de symbiose
